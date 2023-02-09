7320b8802d40e71080a3bcc7c88bd9419d1a13f7
Ana Caroline Nogueira <carolnogueira13@gmail.com>
2023-02-09 13:10:41 -0300

    OrderItem, many-to-many association with extra attributes PART 01

---
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -0,0 +1,97 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+
+import com.educandoweb.course.entities.pk.OrderItemPK;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private OrderItemPK id;
+	
+	private Integer quantity;
+	private Double price;
+	
+	public OrderItem() {
+		
+	}
+
+	/*
+	 Construtor que utliza O order e o product como parâmetros para definir o OrderItem e dentro do construtor esses parâmetros
+	 passados iram modificar o valor no id (OrderItemPK, que funciona como uma chave primária composta de OrderItem)
+	 */
+	public OrderItem(Order order, Product product,Integer quantity, Double price) {
+		super();
+		id.setOrder(order); 
+		id.setProduct(product);
+		this.quantity = quantity;
+		this.price = price;
+	}
+	
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void setProduct(Product product ) {
+		id.setProduct(product);
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+
+}
